32ca535f74b5ec3b8584fce0ef324c54ab3d547a
Irfan Catovic <catovicc84@gmail.com>
2019-11-18 15:27:23 +0100

    Add project files.

---
+++ PogadjanjeRijeci/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PogadjanjeRijeci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            
+
+            char unos;
+
+            string zavrsnaRec;
+            char[] rec = "test".ToCharArray();
+            string test = Console.ReadLine();
+            char[] arr;
+            arr = test.ToCharArray();
+            string pogodak = rec;
+            for (int i = 0; i < rec.Length; i++)
+            {   
+                Console.Write("_ ");
+            }
+            do
+            {
+                Console.WriteLine();
+                unos = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                bool tacno = false;
+                for (int i = 0; i < rec.Length; i++)
+                {
+                    if(pogodak[i] == unos)
+                    {
+                        test[i] = unos;
+                        pogodak += unos;
+                        
+                    }
+                    else
+                    {
+                        Console.Write("_ ");
+                    }
+                }
+                Console.WriteLine();
+
+            } while (unos != '4');
+        }
+    }
+}
---
+++ Zavrsni/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zavrsni
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+
+
+            Console.WriteLine("Unesite rec: "); //unosenje reci
+            string rec = Console.ReadLine();
+            char[] nizRec;
+            nizRec = rec.ToCharArray();
+            char[] copy = nizRec;//kopija reci za proveru
+
+            char[] nizTest = new char[nizRec.Length]; //rec za ispis
+
+            char unos;
+
+            for (int i = 0; i < copy.Length; i++)
+            {
+                Console.Write("_ "); //linija za broj slova
+            }
+            bool tacno = false;
+            do
+            {
+                Console.Write("Unesite slovo: ");
+                unos = Console.ReadKey().KeyChar;
+                Console.WriteLine();
+                for (int indeks = 0;  indeks < copy.Length;  indeks++)
+                {
+                    if (copy[indeks] == unos)
+                    {
+                        Console.WriteLine("Pogodak! "); //provera unosa
+                        nizTest[indeks] = unos;                        
+                        tacno = true;
+                    }
+                }
+                Console.WriteLine(nizTest);
+            } while (unos != '4');
+
+
+            Console.ReadKey();
+        }
+    }
+}
