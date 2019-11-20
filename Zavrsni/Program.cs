83dbebf3322f672d2f9ee3cccd06c53d0b08ef02
Irfan Catovic <catovicc84@gmail.com>
2019-11-20 01:55:45 +0100

    random radi

--- Zavrsni/Program.cs
+++ Zavrsni/Program.cs
@@ -11,21 +11,37 @@ namespace Zavrsni
     {
         static void Main(string[] args)
         {
-            
+            /*if (File.Exists("art.txt"))
+            {
+                foreach (string reci in File.ReadLines("rec.txt"))
+                {
+                    string[] polja = reci.Split(';');
+
+                    Artikli.Add((polja[0], polja[1],
+                        int.Parse(polja[2]), decimal.Parse(polja[3])));
+                }
+            }*/
 
                 string[] reci = File.ReadAllLines("reci.txt");
                 foreach (string reciIzNiza in reci)
                 {
                     
                 }
-           
-            Random nasumicnaRec = new Random();
-            var randomLinijaBroj = nasumicnaRec.Next(0, reci.Length - 1);
-            string linija = reci[randomLinijaBroj];
 
 
             
-            char[] copy = linija;//kopija reci za proveru
+
+            
+
+            Random nasumicnaRec = new Random();
+            var randomLinijaBroj = nasumicnaRec.Next(0, reci.Length - 1);
+            var linija = reci[randomLinijaBroj];
+
+
+            
+            char[] nizRec;
+            nizRec = linija.ToCharArray();
+            char[] copy = nizRec;//kopija reci za proveru
 
             char[] nizTest = new char[nizRec.Length]; //rec za ispis
             //string unosRec;
