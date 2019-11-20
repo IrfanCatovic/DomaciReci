d3dbb3b24ef5a1974d3761cb21d28bf9e77d87fa
Irfan Catovic <catovicc84@gmail.com>
2019-11-20 14:31:42 +0100

    Promena

--- Zavrsni/Program.cs
+++ Zavrsni/Program.cs
@@ -11,58 +11,92 @@ namespace Zavrsni
     {
         static void Main(string[] args)
         {
-            
+            //kako da proverim je li rec ili 
 
-                string[] reci = File.ReadAllLines("reci.txt");
+            string[] reci = File.ReadAllLines("reci.txt");
                 
             Random nasumicnaRec = new Random();
             var randomLinijaBroj = nasumicnaRec.Next(0, reci.Length - 1);
             var linija = reci[randomLinijaBroj];
             
             char[] nizRec;
-            nizRec = linija.ToCharArray();
+            nizRec = linija.ToCharArray(); //linija je random rec iz fajla
             char[] copy = nizRec;//kopija reci za proveru
 
             char[] nizTest = new char[nizRec.Length]; //rec za ispis
-            //string unosRec;
-            char unos;
 
-            for (int i = 0; i < copy.Length; i++)
-            {
-                Console.Write("_ "); //linija za broj slova
-            }
+            string unos;
+            string unos2;
+
             bool tacno = false;
+            char biranje;
             do
             {
+                Meni();
+                biranje = Console.ReadKey().KeyChar;
                 Console.WriteLine();
-                Console.Write("Unesite slovo: ");
-                unos = Console.ReadKey().KeyChar;
-                Console.WriteLine();
-                for (int indeks = 0; indeks < copy.Length; indeks++)
+                bool izlazC1 = false;
+                switch (biranje)
                 {
-                    if (copy[indeks] == unos)
-                    {
-                        Console.WriteLine("Pogodak! "); //provera unosa
-                        nizTest[indeks] = unos;
-                        tacno = true;
-                    }
-                    
+                    case '1':
+                        {
+                            for (int i = 0; i < copy.Length; i++)
+                            {
+                                Console.Write("_ "); //linija za broj slova
+                            }
+                            do
+                            {   
+                                Console.WriteLine();
+                                Console.Write("Unesite slovo: ");
+
+                                unos = Console.ReadLine();
+                                Console.WriteLine();
+                                for (int indeks = 0; indeks < copy.Length; indeks++)
+                                {
+                                    if (copy[indeks] == unos)
+                                    {
+                                        Console.WriteLine("Pogodak! "); //provera unosa
+                                        nizTest[indeks] = unos;
+
+                                    }
+                                }
+                                foreach (char c in nizTest)
+                                {
+                                    if (c != '\0')
+                                    {
+                                        Console.Write(c);
+                                    }
+                                    else
+                                    {
+                                        Console.Write("_ ");
+                                    }
+                                }
+                                if (copy == nizTest)
+                                {
+                                    izlazC1 = true;
+                                }
+                            } while (!izlazC1);   
+                            break;
+                        }
+
+                    case '2':
+
+                        break;
                 }
-                foreach (char c in nizTest)
-                {
-                    if(c != '\0')
-                    {
-                        Console.Write(c);
-                    }
-                    else
-                    {
-                        Console.Write("_ ");
-                    }
-                }
-            } while (unos != '4');
+                
+                
+            } while (biranje != '3');
 
 
             Console.ReadKey();
+        }
+        static void Meni()
+        {
+            Console.WriteLine("1 -- Igraj! \n" +
+                              "2 -- Top Lista: \n" +
+                              "3 -- Izlaz! \n" +
+                              "==================\n" +
+                              "Izaberite opciju: \n");
         }
     }
 }
